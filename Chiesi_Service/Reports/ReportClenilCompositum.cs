1f433f1fe3756780def54753415ba7674c0be9f0
FernandaRachel <fefedel9@hotmail.com>
2018-05-28 11:35:04 -0300

    Addition & HighSpeed updated to receive index as parameter

--- Chiesi_Service/Reports/ReportClenilCompositum.cs
+++ Chiesi_Service/Reports/ReportClenilCompositum.cs
@@ -44,12 +44,12 @@ namespace Chiesi.Reports
             op2 = new ZeroLoadCell(eqtype);
             op3 = new FirstLoading(eqtype, "1º Carregamento de Álcool Etílico Anidro", "1.2", "3");
             op5 = new TempMonitoringClass(eqtype, false);
-            op6 = new AdditionClass(eqtype, "Adição de Excipiente pelo Bocal T2", false, false);
-            op7 = new AdditionClass(eqtype, "Adição de Dipropionato de Baclometasoma", false, false);
-            op8 = new HighSpeedMix(eqtype, "30", "0", "1500", "2000", true, false, false, "5");
+            op6 = new AdditionClass(eqtype, "Adição de Excipiente pelo Bocal T2", false, false,0);
+            op7 = new AdditionClass(eqtype, "Adição de Dipropionato de Baclometasoma", false, false,0);
+            op8 = new HighSpeedMix(eqtype, "30", "0", "1500", "2000", true, false, false, "5", 1);
             op9 = new LowSpeedMix(eqtype, "15", "30", false);
-            op10 = new AdditionClass(eqtype, "Adição de Sulfato de Salbutamol Micromizado", true, false);
-            op11 = new HighSpeedMix(eqtype, "30", "0", "2000", "2500", true, false, false,"20");
+            op10 = new AdditionClass(eqtype, "Adição de Sulfato de Salbutamol Micromizado", true, false,2);
+            op11 = new HighSpeedMix(eqtype, "30", "0", "2000", "2500", true, false, false,"20",1);
             op12 = new TankFinalWeight(eqtype, "", false);
             op13 = new ValveClass(eqtype, "15", true, true, "200", "494", "V8");
             op14 = new EndOfManipulation(eqtype, false, true);
