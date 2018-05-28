3aa2800ed67d2712e33c1a75dce1f136814ba66e
FernandaRachel <fefedel9@hotmail.com>
2018-05-28 13:11:24 -0300

    reading variation & qtds from Recipe Array

--- Chiesi_Service/Loading/SecondLoadingClass.cs
+++ Chiesi_Service/Loading/SecondLoadingClass.cs
@@ -105,40 +105,37 @@ namespace Chiesi.Loading
             checkError();
             // It will search the infos correponding to the specific operation
             var operationInfos = successor.SearchInfoInList(this.eq, this.operationID);
+            var result = operationInfos.ElementAt(index);
 
             bool gerarPdf = false;
-            DateTime keepinidate = DateTime.Now;
             string cellVariation = "";
             string flowvariation = "";
 
             try
             {
+                logAction.writeLog("Iniciando leituras/escrita das tags necessárias");
 
-                eq.Write(StaticValues.FLOWMETERLIMIT, limitFlow);
-                eq.Write(StaticValues.CELLLIMIT, limitCell);
-                Thread.Sleep(300);
+                //LENDO HORA INCIAL
+                logAction.writeLog("Lendo hora inicial do SecondLoading");
+                gli.OutFlowStart = Convert.ToDateTime(result.Hora_0);
 
-                //PEGAR HORA DO PLC
+                //LENDO HORA FINAL
+                logAction.writeLog("Lendo hora final  do SecondLoading");
+                gli.OutFlowEnd = Convert.ToDateTime(result.Hora_1);
 
-                logAction.writeLog("Lendo hora inicial da mistura do SecondLoading");
-                keepinidate = DateTime.Now;
-
-
-                logAction.writeLog("Iniciando leituras das tags necessárias do SecondLoading");
-                cellVariation = eq.Read(StaticValues.TAGVARCELL).Replace(".", ",");
-                flowvariation = eq.Read(StaticValues.TAGVARFLOW).Replace(".", ",");
-                this.flux.ReadPlc(); // inicializa os valores do Flowmeter
-                this.cell.ReadPlc(); // inicializa os valores da LoadingCell
-                this.infos.ReadPlc(); // inicializa os valores da BasicInfo
-                this.gli.ReadPlc(); // inicializa os valores de Glicerol
-
-                //PEGAR HORA DO PLC
-                logAction.writeLog("Lendo hora final da mistura do SecondLoading");
-                gli.OutFlowStart = keepinidate;
-                gli.OutFlowEnd = DateTime.Now;
-                this.infos.Date = gli.OutFlowEnd;
-                // -------------------------------
-                Thread.Sleep(200);
+                //LENDO VARIAÇÕES E QUANTIDADES
+                logAction.writeLog("Iniciando leituras variações e quantidades");
+                gli.GliQty = convert.convertToDouble("result.Param_0", result.Param_0);
+                flux.RealQty = convert.convertToDouble("result.Param_1", result.Param_1);
+                flux.TheoricQty = result.Param_2;
+                cellVariation = result.Param_3.Replace(".", ",");
+                flowvariation = result.Param_4.Replace(".", ",");
+                
+               
+                // Define os novos valores do basic info = assinatura
+                this.infos.Hour = Convert.ToDateTime(result.Hora_1);
+                this.infos.Date = Convert.ToDateTime(result.Date);
+                this.infos.OperatorLogin = result.Asignature;
             }
             catch (Exception e)
             {
